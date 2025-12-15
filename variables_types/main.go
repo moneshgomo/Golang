46cc2871def7d1760c6607e482df07d58e9a5815
MONESHGOMO <moneshd16@gmail.com>
2025-12-15 17:00:21 +0530

    feat: notes

---
+++ variables_types/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// alt shitft f to format the code
+
+func main() {
+	var userName string = "moneshgomo"
+	fmt.Println(userName)
+	fmt.Printf("variable type : %T \n ", userName)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("variable type : %T \n ", isLoggedIn)
+
+}
